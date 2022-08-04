9913fd50c9aba0dc44e7748e2818337e4b3366e3
Yinzorg <grandtio@mail.ru>
2022-08-04 11:59:27 +0400

    My first git commit

---
+++ src/t1.java
@@ -0,0 +1,29 @@
+public class t1{
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson 1");
+
+        byte x = 127;
+        System.out.println(x);
+        short y = -4542;
+        System.out.println(y);
+
+        int a = 45454;
+        System.out.println(a);
+
+        long b = 457L;
+        System.out.println(b);
+
+
+        float aa = 465.4f;
+        double ss = 85.4;
+
+
+        boolean v = true;
+        v = false;
+
+        char t = '%';
+        System.out.println(t);
+    }
+
+}
